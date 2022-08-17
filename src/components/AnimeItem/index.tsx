e2ef32f0e8a565c514b6cc162054fd816cb699e3
HatyushinAS <kakdevonte@yandex.ru>
2022-08-17 07:02:37 +0300

    change layout

--- src/components/AnimeItem/index.tsx
+++ src/components/AnimeItem/index.tsx
@@ -3,8 +3,15 @@ import styles from './AnimeItem.module.scss';
 
 export const AnimeItem: React.FC = () => {
   return (
-    <>
-      <h2>Anime item</h2>
-    </>
+    <div className={styles.root}>
+      <div>
+        <img src={'https://cdn.myanimelist.net/images/anime/4/19644.jpg'} />
+      </div>
+      <p className={styles.title}>Cowboy Bebop</p>
+      <div className={styles.info}>
+        <span>TV</span>
+        <span>1998</span>
+      </div>
+    </div>
   );
 };
--- src/pages/AnimesPage/index.tsx
+++ src/pages/AnimesPage/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { AnimeItem } from '../../components/AnimeItem';
 import styles from './AnimesPage.module.scss';
 
 export const AnimesPage: React.FC = () => {
   return (
     <>
       <h1>AnimesPage</h1>
+      <div className={styles.list}>
+        {[...Array(25)].map((item, index) => (
+          <AnimeItem key={index} />
+        ))}
+      </div>
     </>
   );
 };
