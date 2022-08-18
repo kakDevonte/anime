0545c1c4aed7f2fb5f3a3bf95defe98a61ad4e09
HatyushinAS <kakdevonte@yandex.ru>
2022-08-18 08:46:08 +0300

    added AnimeItem layout

--- src/components/AnimeItem/index.tsx
+++ src/components/AnimeItem/index.tsx
@@ -3,15 +3,48 @@ import styles from './AnimeItem.module.scss';
 import { AnimeType } from '../../redux/animes/types';
 
 export const AnimeItem: React.FC<AnimeType> = (props) => {
+  const infoString = `${props.type},${props.aired.prop.from.year}
+    | ${props.status.split(' ')[0]}
+    | ${props.episodes} ${props.episodes > 1 ? 'eps' : 'ep'} ${props.duration}`;
+
   return (
     <div className={styles.root}>
-      <div className={styles.image}>
-        <img src={props.images.webp.image_url} />
+      <div className={styles.title}>
+        <p className={styles.title}>{props.title}</p>
       </div>
-      <p className={styles.title}>{props.title}</p>
       <div className={styles.info}>
-        <span>{props.type}</span>
-        <span>{props.aired.prop.from.year}</span>
+        <span>{infoString}</span>
+      </div>
+      <div className={styles.genre}>
+        <ul className={styles.valueList}>
+          {props.genres.map((item) => (
+            <li key={item.mal_id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.synopsis}>
+        <div className={styles.image}>
+          <img src={props.images.webp.image_url} />
+        </div>
+        <div>
+          <div className={styles.item}>
+            <div className={styles.text}>{props.synopsis}</div>
+          </div>
+          {props.themes.length > 0 && (
+            <div className={styles.item}>
+              <p>Theme: </p>
+              <ul className={styles.valueList}>
+                {props.themes.map((item) => (
+                  <li key={item.mal_id}>{item.name}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          <div className={styles.item}>
+            <p>Studio: </p>
+            <span className={styles.studio}>{props.studios[0].name}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
